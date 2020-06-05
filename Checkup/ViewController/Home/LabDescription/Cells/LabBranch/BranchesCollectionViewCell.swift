0a70c333811b244e78017f97e02bd4a62849a15e
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-06-05 03:22:30 +0200

    employee constraints

--- Checkup/ViewController/Home/LabDescription/Cells/LabBranch/BranchesCollectionViewCell.swift
+++ Checkup/ViewController/Home/LabDescription/Cells/LabBranch/BranchesCollectionViewCell.swift
@@ -24,9 +24,29 @@ class BranchesCollectionViewCell: UICollectionViewCell {
         requestBtnOutlet.layer.cornerRadius = 6
         requestBtnOutlet.layer.borderWidth = 2
         requestBtnOutlet.layer.borderColor = UIColor.white.cgColor
-    
- 
- 
+//    backgroundColor = .clear
+//
+//
+//      let blurEffect = UIBlurEffect(style: .regular)
+//      let blurView = UIVisualEffectView(effect: blurEffect)
+//      blurView.translatesAutoresizingMaskIntoConstraints = false
+//      insertSubview(blurView, at: 0)
+//        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+//           self.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+//           self.layer.shadowOpacity = 1.0
+//           self.layer.shadowRadius = 0.0
+//           self.layer.masksToBounds = false
+////           self.layer.cornerRadius = 4.0
+//        self.tintColor = .clear
+//      NSLayoutConstraint.activate([blurView.heightAnchor.constraint(equalTo: heightAnchor) , blurView.widthAnchor.constraint(equalTo:widthAnchor)])
+//
+//        layer.shadowPath =  UIBezierPath(rect: self.bounds).cgPath
+//layer.shadowRadius = 14
+// layer.shadowOffset = .zero
+// layer.shadowOpacity = 0.5
+// layer.cornerRadius = 5
+// layer.borderWidth = 5
+// layer.borderColor = UIColor.white.cgColor
  }
 
 
