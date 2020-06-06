89b10dad692854b71fa980928829bb4bb894f148
Aya hamdy <35396186+Ayahamdy97@users.noreply.github.com>
2020-06-05 20:01:13 -0700

    rating

---
+++ Checkup/ViewController/Rating/cells/ButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonTableViewCell.swift
+//  Checkup
+//
+//  Created by Aya on 6/5/20.
+//  Copyright © 2020 Hassan Khamis. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Checkup/ViewController/Rating/cells/RatingTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  RatingTableViewCell.swift
+//  Checkup
+//
+//  Created by Aya on 6/5/20.
+//  Copyright © 2020 Hassan Khamis. All rights reserved.
+//
+
+import UIKit
+
+class RatingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ratingView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
