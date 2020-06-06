1da5aed9d12bbbcfd4b582b7c41723abfac19556
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-06-06 21:08:35 +0200

    chat messaging fix notification

--- Checkup/ViewController/Rating/RatingTableViewController.swift
+++ Checkup/ViewController/Rating/RatingTableViewController.swift
@@ -21,46 +21,37 @@ class RatingTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
  
            let cell = UITableViewCell()
                 if(indexPath.row == 0){
-                  let ratingCell = tableView.dequeueReusableCell(withIdentifier: "ratingCell", for: indexPath) as! RatingTableViewCell
-                    
-                    ratingCell.ratingView.layer.shadowPath =  UIBezierPath(rect: cell.bounds).cgPath
+                  let ratingCell = tableView.dequeueReusableCell(withIdentifier: "rating", for: indexPath) as! RatingTableViewCell
+
+                    ratingCell.ratingView.layer.shadowPath =  UIBezierPath(rect: ratingCell.bounds).cgPath
                     ratingCell.ratingView.layer.shadowRadius = 4
                     ratingCell.ratingView.layer.shadowOffset = .zero
                     ratingCell.ratingView.layer.shadowOpacity = 0.05
                     ratingCell.ratingView.layer.cornerRadius = 5
                     ratingCell.ratingView.layer.borderWidth = 5
                     ratingCell.ratingView.layer.borderColor = UIColor.white.cgColor
-                    
+
                  return ratingCell
-                 
-             }else if(indexPath.row==1){
+
+             }else
+        if(indexPath.row==1){
                 let addNoteCell = tableView.dequeueReusableCell(withIdentifier: "addNoteCell", for: indexPath) as! NoteTableViewCell
 
                     
                     return addNoteCell
                  
-             }else if(indexPath.row==2){
+             }else {
               let buttonCell = tableView.dequeueReusableCell(withIdentifier: "buttonCell", for: indexPath) as! ButtonTableViewCell
                 
                return buttonCell
              }
                          
-               return cell
+//               return cell
 
         
 
