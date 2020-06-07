e065f3b687cc1439493383d085e8fa4738ad1650
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-06-07 05:38:27 +0200

    rating

--- Checkup/ViewController/Rating/RatingTableViewController.swift
+++ Checkup/ViewController/Rating/RatingTableViewController.swift
@@ -10,18 +10,45 @@ import UIKit
 
 class RatingTableViewController: UITableViewController {
 
+    @IBOutlet weak var commentOutlet: UITextView!
+    @IBOutlet weak var ratingView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
       //  tableView.backgroundView = UIImageView(image: UIImage(named: "newmicroscope (1)"))
-        let imageView = UIImageView(image: UIImage(named:  "newmicroscope (1)"))
+        commentOutlet.layer.borderWidth = 0.5
+        commentOutlet.layer.borderColor = UIColor.gray.cgColor
+        let imageView = UIImageView(image: UIImage(named:  "newmicroscope (1)"))//
         imageView.alpha = 0.4
         self.tableView.backgroundView = imageView
+        ratingView.layer.shadowPath =  UIBezierPath(rect:ratingView.bounds).cgPath
+                          
+                           ratingView.layer.shadowRadius = 10
+                           ratingView.layer.shadowOffset = .zero
+                           ratingView.layer.shadowOpacity = 0.1
+                           ratingView.layer.cornerRadius = 10
+                 //          ratingView.layer.borderWidth = 5
+                   //       ratingView.layer.borderColor = UIColor.white.cgColor
+        
+        
+        
+        
+        
+         ratingView.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: .extraLight)
+           let blurView = UIVisualEffectView(effect: blurEffect)
+           blurView.translatesAutoresizingMaskIntoConstraints = false
+           ratingView.insertSubview(blurView, at: 0)
+           NSLayoutConstraint.activate([blurView.heightAnchor.constraint(equalTo: ratingView.heightAnchor) , blurView.widthAnchor.constraint(equalTo:ratingView.widthAnchor)])
+        
     }
 
     // MARK: - Table view data source
-
     
+/*
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
  
            let cell = UITableViewCell()
@@ -56,6 +83,7 @@ class RatingTableViewController: UITableViewController {
         
 
     }
+    */
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.backgroundColor = UIColor(white : 1 , alpha: 0.5)
     }
